76cf81da887869323586966591c3b433f4de8f19
ConalG <conal-glynnmartin@hotmail.com>
2021-11-29 11:06:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,32 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () =>{
+    return Math.floor(Math.random() * 10);
+  }
+  
+  const compareGuesses = (humanGuess, comGuess, target) =>{
+    humanDiff = Math.abs(humanGuess - target); 
+    comGuess = Math.abs(comGuess - target);
+  
+    if(humanDiff <= comGuess ){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  
+  const updateScore = winner => {
+    if(winner === `human`){
+      humanScore += 1;
+    }
+    else if(winner === `computer`){
+      computerScore += 1;
+    }
+  }
+  
+  const advanceRound = () =>{
+    currentRoundNumber += 1
+  }
 
